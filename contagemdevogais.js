d6ca952de7551d6a01737af5ea00cb8641c0ba3d
Julia Martins <juliamartins.td2.senai@gmail.com>
2024-03-19 15:29:02 -0300

    metodo filter para filtrar as vogais e o metodo length para falar quantos elementos são vogais

--- contagemdevogais.js
+++ contagemdevogais.js
@@ -4,5 +4,9 @@ let nome = "julia";
 let separado = nome.split("");
 
 //declarei um array com as vogais
-let vogais = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I' ,'O', 'U'];
+let vogais = ['a', 'e', 'i', 'o', 'u'];
 
+//metodo de filtrar as vogais
+let quantasvogais = separado.filter(letras => vogais.includes(letras));
+console.log("A quantidade de vogais é:" + quantasvogais.length);
+console.log(quantasvogais);
