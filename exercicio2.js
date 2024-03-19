4368b24cc92183873f6be07bf92f2efc8fb29db1
Julia Martins <juliamartins.td2.senai@gmail.com>
2024-03-19 17:16:50 -0300

    resolução utilizando a estrutura de reptição FOR

--- exercicio2.js
+++ exercicio2.js
@@ -1,1 +1,45 @@
 //Desafio de contagem de Vogais, Consoantes e espaços
+let vogais = ['a', 'e', 'i', 'o', 'u'];
+
+let consoante = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'x', 'y', 'w', 'z'];
+
+let nome = 'Julia de Arruda Martins';
+
+nome = nome.toLowerCase();
+
+let arrayLetras = nome.split('');
+
+let arrayVogais = [];
+let arrayConsoantes = [];
+let arrayEspaço = [];
+
+//Vogais
+for(let i = 0; i < arrayLetras.length; i++){
+    if(vogais.includes(arrayLetras[i])){
+        arrayVogais.push(arrayLetras[i]);
+    }
+}
+console.log('nome:');
+console.log('vogais encontradas:', arrayVogais.toString());
+console.log('total de vogais:' + arrayVogais.length);
+
+
+//Consoantes
+for(let j = 0; j < arrayLetras.length; j++){
+    if(consoante.includes(arrayLetras[j])){
+        arrayConsoantes.push(arrayLetras[j]);
+    }
+}
+
+console.log('nome:');
+console.log('consoantes encontrados:', arrayConsoantes.toString());
+console.log('total de consoantes:' + arrayConsoantes.length);
+
+//espaços
+for(let k = 0; k < arrayLetras.length; k++){
+        arrayEspaço.push(arrayLetras[k]);
+}
+
+console.log('nome:');
+console.log('espaços encontrados:', arrayEspaço.toString());
+console.log('total de espaço:' + arrayEspaço.length);
